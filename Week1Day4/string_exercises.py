09e3979d76e2dd0276471ccae2c78d8982d9b862
Eli Zhang <zhang.eli@hotmail.com>
2019-04-20 18:23:03 -0400

    string exercises

---
+++ Week1Day4/string_exercises.py
@@ -0,0 +1,51 @@
+### Uppercase a String ###
+
+# string = input('Enter a string: ')
+# print(string.upper())
+
+### Capitalize a String ###
+
+# string = input('Enter a string: ')
+# print(string.capitalize())
+
+### Reverse a String ###
+
+# string = input('Enter a string: ')
+# str = "" 
+# for i in string: 
+#     str = i + str
+
+# print(str)
+
+### Leetspeak ###
+
+# string = input('Enter a sentence: ')
+# leet = ""
+
+# for letter in string:
+#     if letter == 'A' or letter == 'a':
+#         leet += '4'
+#     elif letter == 'E' or letter == 'e':
+#         leet += '3'
+#     elif letter == 'G' or letter == 'g':
+#         leet += '6'
+#     elif letter == 'L' or letter == 'l':
+#         leet += '1'
+#     elif letter == 'O' or letter == 'o':
+#         leet += '0'
+#     elif letter == 'S' or letter == 's':
+#         leet += '5'
+#     elif letter == 'T' or letter == 't':
+#         leet += '7'
+#     else:
+#         leet += letter
+
+# print(leet)
+
+### Long-long Vowels ###
+
+string = input('Enter a word: ')
+long = ""
+
+for letter in string:
+   
